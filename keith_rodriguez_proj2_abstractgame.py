ccb043f7ffa01811f4eda6623bdd6b92e5a3a2c4
ReverseK <reversekeith@gmail.com>
2016-12-19 22:38:35 -0600

    Revert "Metagrid class"
    This reverts commit 872a96ca58a2cbf8f96d83afaec33460c648ec98.

--- keith_rodriguez_proj2_abstractgame.py
+++
@@ -1,9 +0,0 @@
-class Game:
-
-    @staticmethod
-    def player_turn(used_cells, turn_number, player, game_grid):
-        pass
-
-    @staticmethod
-    def is_game_over(player_value, game_grid, turn_number):
-        pass
